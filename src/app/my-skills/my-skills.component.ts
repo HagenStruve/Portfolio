c5032d1dea2badd844b562a68c114da353847b36
HagenStruve <111054317+HagenStruve@users.noreply.github.com>
2023-01-09 16:56:06 +0100

    genraty skill cards with ngfor

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -7,47 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySkillsComponent implements OnInit {
 
+  public mySkills = [
+    {
+      "skillName":"Angular",
+      "skillImg": "angular"
+    },
+    {
+      "skillName":"Git",
+      "skillImg": "Git-Icon-1788C 2"
+    },
+    {
+      "skillName":"Java Script",
+      "skillImg": "java script"
+    },
+    {
+      "skillName":"HTML/CSS",
+      "skillImg": "Group 205"
+    },
+    {
+      "skillName":"Scrum",
+      "skillImg": "Group 226"
+    },
+    {
+      "skillName":"Database",
+      "skillImg": "icons8-database-52 2"
+    },
+    {
+      "skillName":"Rest API",
+      "skillImg": "icons8-rest-api-80 2"
+    },
+    {
+      "skillName":"Disign Thinking",
+      "skillImg": "Group 20"
+    },
+    {
+      "skillName":"Test automation",
+      "skillImg": "icons8-test-lab-96 2"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
-    let mySkills = [
-      {
-        "skillName":"Angular",
-        "skillImg": "assets/angular.png"
-      },
-      {
-        "skillName":"Git",
-        "skillImg": "assets/Git-Icon-1788C 2.png"
-      },
-      {
-        "skillName":"Java Script",
-        "skillImg": "assets/java script.png"
-      },
-      {
-        "skillName":"HTML/CSS",
-        "skillImg": "assets/Group 205.png"
-      },
-      {
-        "skillName":"Scrum",
-        "skillImg": "assets/Group 226.png"
-      },
-      {
-        "skillName":"Database",
-        "skillImg": "assets/icons8-database-52 2.png"
-      },
-      {
-        "skillName":"Rest API",
-        "skillImg": "assets/icons8-rest-api-80 2.png"
-      },
-      {
-        "skillName":"Disign Thinking",
-        "skillImg": "assets/Group 20.png"
-      },
-      {
-        "skillName":"Test automation",
-        "skillImg": "assets/icons8-test-lab-96 2.png"
-      }
-    ]
+    
   }
 
 }
