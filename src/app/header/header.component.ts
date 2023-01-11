05aed8e4470aa0d70d2f98319ef7effcc82d97ef
HagenStruve <111054317+HagenStruve@users.noreply.github.com>
2023-01-11 22:01:51 +0100

    add project images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, Renderer2, ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,26 +6,6 @@ import { Component, HostListener, OnInit, Renderer2, ElementRef} from '@angular/
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  lastScrollTop = 0;
-  isVisible = true;
-  renderer:any;
-  el:any;
-
-  @HostListener('window:scroll')
-  onWindowScroll() {
-    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollPosition > this.lastScrollTop && this.isVisible) {
-        this.renderer.addClass(this.el.nativeElement.querySelector('.header'), 'hide');
-        this.isVisible = false;
-    } else {
-      if(scrollPosition < this.lastScrollTop && !this.isVisible){
-        this.renderer.removeClass(this.el.nativeElement.querySelector('.header'), 'hide');
-        this.isVisible = true;
-      }
-    }
-    this.lastScrollTop = scrollPosition;
-  }
 
   constructor() {  }
   ngOnInit(): void {
