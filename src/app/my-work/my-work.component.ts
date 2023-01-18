9c635c37f37e4c5abb3adb895c2ae5fc2ee5fc02
HagenStruve <111054317+HagenStruve@users.noreply.github.com>
2023-01-18 11:11:18 +0100

    try filter function

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -7,14 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyWorkComponent implements OnInit {
 
-projects = [];
-tool = [];
+  projects = [
+    {
+      "js": "js",
+      "image": "assets/Elpolloloco.png",
+      "name": "El Pollo Loco",
+      "text": "Java Script -baded jump and run game",
+      "url": "https://hagen-struve.developerakademie.net/Developer%20Akademie/module/modul_11/El_Pollo_Loco/index.html",
+      "urlGithub": "https://github.com/HagenStruve/El_Pollo_Loco"
+    },
+    {
+      "js": "js",
+      "image": "assets/Join.png",
+      "name": "Join",
+      "text": "Java Script -Kanbanboard",
+      "url": "https://hagen-struve.developerakademie.net/Developer%20Akademie/module/modul_10/Join/html/summary.html",
+      "urlGithub": "https://github.com/HagenStruve/Join-Gruppenarbeit"
+    },
+    {
+      "js": "js",
+      "image": "assets/Pokedex.png",
+      "name": "Pokedex",
+      "text": "Java Script -build with API",
+      "url": "https://hagen-struve.developerakademie.net/Developer%20Akademie/module/modul_9/Projekte/Pokedex/index.html",
+      "urlGithub": "https://github.com/HagenStruve/Pokedex"
+    },
+    {
+      "angular": "angular",
+      "image": "assets/Portfolio.png",
+      "name": "Portfolio",
+      "text": "This is Angular project",
+      "url": "https://hagen-struve.de",
+      "urlGithub": "https://github.com/HagenStruve/Portfolio"
+    },
+  ];
+  tool = [];
 
-all(){}
+  all() { }
 
-angular(){}
+  angular() {
 
-javaScript(){}
+      let jsSort = this.projects.filter( =>(item: { js: string; }) {
+        return item.js == "js";
+      });
+      console.log("Returned string is : " + jsSort);
+  }
+
+  javaScript() { }
 
 
   constructor() { }
