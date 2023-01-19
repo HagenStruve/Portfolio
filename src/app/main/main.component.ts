15f60d36edb7abd4cf4f2692dac81cb8cef7d02d
HagenStruve <111054317+HagenStruve@users.noreply.github.com>
2023-01-19 16:27:28 +0100

    try scrollanimation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+   ngOnInit(): void {
+   }
+
+   isElementInViewport(element:any) {
+    var rect = element.getBoundingClientRect();
+    return (
+     rect.top >= 0 &&
+     rect.left >= 0 &&
+     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+   }
+   
+  //  var elements = document.querySelectorAll(".mein-element");
+    
+   callbackFunc(this: any) {
+    var elements = document.querySelectorAll(".mein-element");
+    for (var i = 0; i < elements.length; i++) {
+     if (this.isElementInViewport(elements[i])) {
+    elements[i].classList.add("visible");
+   }
+   
+    /* Else-Bedinung entfernen, um .visible nicht wieder zu löschen, wenn das Element den Viewport verlässt. */
+     else { 
+      elements[i].classList.remove("visible");
+     }
+    }
+  }
+   
+  addEventListener(type: "load", callbackFunc: any);
+  addEventListener(type: "scroll", callbackFunc: any);
+    
+  //  window.addEventListener("load", this.callbackFunc);
+  //  window.addEventListener("scroll", this.callbackFunc);
+  
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void { }
 
 }
